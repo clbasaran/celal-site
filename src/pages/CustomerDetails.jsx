3b02bbc161209cae1d3d7c8468088ea28884a5e5
celal başaran <celalba78@icloud.com>
2025-06-02 21:55:17 +0300

    Repository temizlendi - gereksiz development dosyaları silindi

--- src/pages/CustomerDetails.jsx
+++
@@ -1,8 +0,0 @@
-                  <div className="p-6">
-                    <AddDeliveryForm 
-                      customerId={parseInt(id)}
-                      onSubmit={handleDeliverySubmit}
-                      loading={submittingDelivery}
-                      availableProducts={customerStocks}
-                    />
-                  </div> 
